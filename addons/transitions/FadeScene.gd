edd332520315fc9f0aef201a182bcc8435aea4f4
nightblade9 <nightbladecodes@gmail.com>
2020-10-13 20:56:21 -0400

    Initial import of code from RPG code-base Added a couple of interesting transition images

---
+++ addons/transitions/FadeScene.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+signal fade_done
+
+export var fade_time_seconds:int = 1
+
+var _total_time:float = 0
+var _enabled = false
+
+func start():
+	_enabled = true
+
+func _process(delta):
+	if not _enabled:
+		return
+		
+	if _total_time >= fade_time_seconds:
+		emit_signal("fade_done")
+		queue_free()
+		
+	_total_time += delta
+	var fade_amount = abs(sin(_total_time))
+	$Sprite.material.set_shader_param("dissolve_amount", fade_amount)
+
+func set_texture(texture:ImageTexture) -> void:
+	$Sprite.material.set_shader_param("dissolve_texture", texture)
