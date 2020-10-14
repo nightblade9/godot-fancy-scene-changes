2a76a590481600fabacf13aafb4558d57a017ca9
nightblade9 <nightbladecodes@gmail.com>
2020-10-13 21:58:16 -0400

    Proper icon, sort of.

--- Scenes/ManualTest1.gd
+++ Scenes/ManualTest1.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 const ManualTest2 = preload('res://Scenes/ManualTest2.tscn')
-const CONICAL_IMAGE = preload("res://addons/transitions/images/conical.png")
+const DISSOLVE_IMAGE = preload("res://addons/transitions/images/blurry-noise.png")
 
 func _unhandled_key_input(event):
 	if event.pressed:
-		Transitions.change_scene(ManualTest2.instance(), Transitions.FadeType.CrossFade, 1)
+		Transitions.change_scene(ManualTest2.instance(), Transitions.FadeType.Blend, 1, DISSOLVE_IMAGE)
--- Scenes/ManualTest2.gd
+++ Scenes/ManualTest2.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
 var ManualTest1 = load('res://Scenes/ManualTest1.tscn')
-const DISSOLVE_IMAGE = preload("res://addons/transitions/images/blurry-noise.png")
 
 func _unhandled_key_input(event):
 	if event.pressed:
-		Transitions.change_scene(ManualTest1.instance(), Transitions.FadeType.Blend, 1, DISSOLVE_IMAGE)
+		Transitions.change_scene(ManualTest1.instance(), Transitions.FadeType.CrossFade, 1)
