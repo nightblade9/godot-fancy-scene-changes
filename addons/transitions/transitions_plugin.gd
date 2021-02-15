c9b97ed33ea53317fa5f8abb758ee6351838d6f5
nightblade9 <nightbladecodes@gmail.com>
2021-02-15 12:23:54 -0500

    Try to fix broken plugin

---
+++ addons/transitions/transitions_plugin.gd
@@ -0,0 +1,11 @@
+tool
+extends EditorPlugin
+
+func _enter_tree():
+    # Initialization of the plugin goes here
+    pass
+
+func _exit_tree():
+    # Clean-up of the plugin goes here
+    # Always remember to remove it from the engine when deactivated
+    pass
