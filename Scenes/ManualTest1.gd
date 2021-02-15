61f1fa03a20117c7f642201b855bd66c1f3900e5
nightblade9 <nightbladecodes@gmail.com>
2021-02-15 12:24:42 -0500

    Remove demo materials

--- Scenes/ManualTest1.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-const ManualTest2 = preload('res://Scenes/ManualTest2.tscn')
-const DISSOLVE_IMAGE = preload("res://addons/transitions/images/blurry-noise.png")
-
-func _unhandled_key_input(event):
-	if event.pressed:
-		Transitions.change_scene(ManualTest2.instance(), Transitions.FadeType.Blend, 1, DISSOLVE_IMAGE)
--- Scenes/ManualTest2.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var ManualTest1 = load('res://Scenes/ManualTest1.tscn')
-
-func _unhandled_key_input(event):
-	if event.pressed:
-		Transitions.change_scene(ManualTest1.instance(), Transitions.FadeType.CrossFade, 1)
